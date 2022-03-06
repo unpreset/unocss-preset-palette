39d8edb37816773d796750a9f5f6dabe690433c1
kritsu <kritsu@qq.com>
2022-03-06 16:49:01 +0800

    fix build.

--- vite.config.ts
+++ vite.config.ts
@@ -8,17 +8,17 @@ import { presetUno } from "unocss"
 import presetPalette from "./src"
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode})=>({
+export default defineConfig(({ mode }) => ({
 	base: "/",
-	
+
 	plugins: [
 		jsx(),
 		uno({
 			presets: [
 				presetUno(),
 				presetPalette({
-					colorMode:{
-						attribute:"data-theme"
+					colorMode: {
+						attribute: "data-theme"
 					},
 					cssVarName(name) {
 						return name
@@ -28,16 +28,16 @@ export default defineConfig(({mode})=>({
 						light: {
 							light: "#f7f8f9",
 							dark: "#1a1a1a",
-                            cafe:"#ffa500",
+							cafe: "#ffa500",
 						},
 						dark: {
 							light: "#1a1a1a",
 							dark: "#ffffffde",
-                            cafe:"#00a5ff"
+							cafe: "#00a5ff"
 						},
 						neutral: {
 							light: "#e3e4e5",
-							dark:"#232323",
+							dark: "#232323",
 							cafe: "#ffa5a5"
 						}
 					}
@@ -47,15 +47,16 @@ export default defineConfig(({mode})=>({
 	],
 	build: {
 		lib:
-		  mode === 'demo'
-			? false
-			: {
-				entry: './src/index.ts',
-				formats: ['es',"cjs"],
-				fileName: (format) => `index.${format}.js`
-			},
+			mode === 'demo'
+				? false
+				: {
+					entry: './src/index.ts',
+					formats: ['es', "cjs"],
+					fileName: (format) => format == "cjs" ? 'index.cjs' : `index.js`
+
+				},
 		rollupOptions: {
-		  external: ['unocss','@unocss/preset-mini'],
+			external: ['unocss', '@unocss/preset-mini'],
 		},
-	  },
+	},
 }))
