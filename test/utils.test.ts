71773a2b9cb7e83d5fc08f5dcfd2ab70ab14efea
chizukicn <chizukicn@outlook.com>
2023-09-09 15:39:26 +0800

    chore: add ci

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,5 +1,5 @@
-import { rgb2hsl } from "src/utils";
 import { expect, it } from "vitest";
+import { rgb2hsl } from "../src/utils";
 
 it("rgb2hsl", () => {
   expect(rgb2hsl([255, 0, 0])).toEqual([0, 1, 0.5]);
